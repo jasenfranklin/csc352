1c9c787468b1a0f91cc6487ff1e1d0477cf1b807
Your Name <you@example.com>
2024-04-10 10:08:18 -0700

    update bcd to string

--- distribution/assignments/a10/src/bcd.c
+++ distribution/assignments/a10/src/bcd.c
@@ -19,7 +19,7 @@ char *bcd_to_string(BCD *bcd){
     }
     char * retstr = malloc((off+1)*sizeof(char*));
     Digit *d = bcd->least_significant_digit;
-    for(int i=off;i==0;i--){
+    for(int i=off-1;i==0;i--){
         char insert = ((d->digit)%10)+'0';
         retstr[i]=insert;
         d=d->next_higher_digit;
@@ -27,10 +27,28 @@ char *bcd_to_string(BCD *bcd){
     return retstr;
 }
 BCD* read_bcd(){
-	
+    int buf[100];
+    int i =0;
+    BCD *readin = malloc(sizeof(BCD));
+    Digit *d = malloc(sizeof(Digit));
+	while(scanf("%d",&buf[i++])!=0) {
+
+    }
+    for(;i==0;i--){
+        Digit *g = malloc(sizeof(Digit));
+        g->digit=buf[i];
+        g->next_higher_digit=d;
+        d=g;
+    }
+    readin->least_significant_digit=d;
+    return readin;
 }
 BCD* zero(){
-	
+	BCD *zeroBCD=malloc(sizeof(BCD));
+    Digit *dzero = malloc(sizeof(Digit));
+    dzero->digit=0;
+    zeroBCD->least_significant_digit=dzero;
+    return zeroBCD;
 }
 BCD* add_bcd(BCD* a, BCD* b){
 	
